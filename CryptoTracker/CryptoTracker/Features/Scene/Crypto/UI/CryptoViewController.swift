8893e8d70260741fab5bf1841a39f5d43b8ffd6c
Thiago <Thiago Monteiro>
2023-06-15 13:27:42 -0300

    Created Scene view controller.

--- CryptoTracker/CryptoTracker/Features/Scene/Crypto/UI/CryptoViewController.swift
+++ CryptoTracker/CryptoTracker/Features/Scene/Crypto/UI/CryptoViewController.swift
@@ -4,5 +4,20 @@ final class CryptoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+}
+
+extension CryptoViewController: ViewConfiguration {
+    func buildViewHierarchy() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+    
+    func viewConfiguration() {
+        view.backgroundColor = .systemBackground
     }
 }
