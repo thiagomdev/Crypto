522d0ff7acc4d91a7ad12559b67720fe6ff9efa0
Thiago <Thiago Monteiro>
2023-06-15 14:28:25 -0300

    Created symbol init insise coin model.

--- CryptoTracker/CryptoTracker/Features/Scene/Crypto/Domain/CoinModel.swift
+++ CryptoTracker/CryptoTracker/Features/Scene/Crypto/Domain/CoinModel.swift
@@ -1,9 +1,13 @@
 import UIKit
 
 struct CoinModel {
-    var symbol: String
-    var image: UIImage
-    var price: Float
-    var amount: Float
-    var historicalData: [Double]
+    var symbol: String = ""
+    var image: UIImage = UIImage()
+    var price: Float = 0.0
+    var amount: Float = 0.0
+    var historicalData: [Double] = []
+    
+    init(symbol: String) {
+        self.symbol = symbol
+    }
 }
