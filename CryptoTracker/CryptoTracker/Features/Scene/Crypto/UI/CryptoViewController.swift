c5c634d39daadf1d6b21315bb6c71afd205a67f6
Thiago <Thiago Monteiro>
2023-06-15 13:40:03 -0300

    Created UITableView.

--- CryptoTracker/CryptoTracker/Features/Scene/Crypto/UI/CryptoViewController.swift
+++ CryptoTracker/CryptoTracker/Features/Scene/Crypto/UI/CryptoViewController.swift
@@ -2,19 +2,59 @@ import UIKit
 
 final class CryptoViewController: UIViewController {
 
+    // MARK: - Components
+    private lazy var cryptoTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 80
+        tableView.backgroundColor = .white
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.delegate = self
+        tableView.dataSource = self
+//        tableView.register(ContactCell.self, forCellReuseIdentifier: ContactCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
     }
+    
+    // MARK: - Methods
+    
+    // MARK: - Selectors
 }
 
+// MARK: - UITableViewDataSource
+extension CryptoViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension CryptoViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - ViewConfiguration
 extension CryptoViewController: ViewConfiguration {
     func buildViewHierarchy() {
-        
+        view.addSubview(cryptoTableView)
     }
     
     func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            cryptoTableView.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 1),
+            cryptoTableView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: cryptoTableView.trailingAnchor, multiplier: 1),
+            view.bottomAnchor.constraint(equalToSystemSpacingBelow: cryptoTableView.bottomAnchor, multiplier: 1)
+        ])
     }
     
     func viewConfiguration() {
