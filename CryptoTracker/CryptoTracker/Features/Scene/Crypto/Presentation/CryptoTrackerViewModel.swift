34331d1abd6db82463f638b9fe1cfd3262099419
Thiago <Thiago Monteiro>
2023-06-15 14:29:19 -0300

    Created counter model, protocol and some logics to make a coin.

--- CryptoTracker/CryptoTracker/Features/Scene/Crypto/Presentation/CryptoTrackerViewModel.swift
+++ CryptoTracker/CryptoTracker/Features/Scene/Crypto/Presentation/CryptoTrackerViewModel.swift
@@ -1,16 +1,27 @@
 import Foundation
 
+protocol CryptoTrackerViewModelProtocol {
+    func showSymbols()
+}
+
 final class CryptoTrackerViewModel {
-    private var model: CoinModel
+    private var model: [CoinModel]
     private let symbols = ["BTC", "ETH", "LTC"]
     
-    init(model: CoinModel) {
+    init(model: [CoinModel]) {
         self.model = model
     }
     
+    var count: Int {
+        model.count
+    }
+}
+
+extension CryptoTrackerViewModel: CryptoTrackerViewModelProtocol {
     func showSymbols() {
         symbols.forEach { symbol in
-            model.symbol = symbol
+            let coin = CoinModel(symbol: symbol)
+            model.append(coin)
         }
     }
 }
